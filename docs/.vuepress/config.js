207ea0f4baf27dd625b2c73c1eaf6e9e4e8c09c9
‘AFlatBoat’ <1766249422@qq.com>
2022-09-19 23:31:13 +0800

    提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: '笔记',
+  description: 'Java相关技术笔记',
+  themeConfig: {
+    nav: require('./nav'),
+    sidebar: require('./sidebar')
+  }
+}
---
+++ docs/.vuepress/sidebar.js
@@ -0,0 +1,7 @@
+module.exports = {
+    '/senior-js/jquery/': require('../senior-js/jquery/sidebar'),
+    '/senior-js/underscore/': require('../senior-js/underscore/sidebar')
+}
+
+
+
